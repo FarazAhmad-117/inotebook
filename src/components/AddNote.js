cbee80ed025e12dfd97b0ee14f6430ffd0b652c8
FarazAhmad-117 <su92-bscsm-s23-117@superior.edu.pk>
2023-11-03 15:17:18 +0500

    Final Commit

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -21,16 +21,16 @@ export default function AddNote(props) {
                 {/* // A form to add Notes */}
                 <form className='my-3'>
                     <div className="mb-3">
-                        <label htmlFor="title" className="form-label">Title </label>
-                        <input type="text" className="form-control" id="title" name='title' aria-describedby="emailHelp" value={note.title} onChange={handleChange} minLength={5} required aria-required />
+                        <label htmlFor="title" className="form-label"><b>Title:</b></label>
+                        <input type="text" className="form-control" placeholder='Enter title here' id="title" name='title' aria-describedby="emailHelp" value={note.title} onChange={handleChange} minLength={5} required aria-required />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="description" className="form-label">Description</label>
+                        <label htmlFor="description" className="form-label"><b>Description:</b></label>
                         <textarea className="form-control" placeholder="Enter description here" id="description" name='description' value={note.description} onChange={handleChange} minLength={5} required aria-required ></textarea>
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="tag" className="form-label">Tag</label>
-                        <input type="text" className="form-control" id="tag" name='tag' value={note.tag} onChange={handleChange} minLength={3} required aria-required />
+                        <label htmlFor="tag" className="form-label"><b>Tag:</b></label>
+                        <input type="text" className="form-control" id="tag" name='tag' placeholder='Enter tag here' value={note.tag} onChange={handleChange} minLength={3} required aria-required />
                     </div>
                     <button disabled={note.title.length<5 || note.description.length<5 || note.tag.length<3} type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
                 </form>
